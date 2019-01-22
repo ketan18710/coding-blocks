122425f4b89e3783191c68937826b5a2b0cc27ad
KetanVerma <Ketan18710@gmail.com>
2019-01-22 12:36:48 +0530

    ffdf

---
+++ Lecture 11 .20-01-19/weather api/main.js
@@ -0,0 +1,15 @@
+// let inp = document.getElementById('city_name');
+// let btn  = document.getElementById('btn');
+// let dis  = document.getElementById('display');
+// btn.onclick = function(){
+
+// } 
+$(document).ready(function(){
+    let inp = $('#city_name');
+    let btn = $('#btn');
+    let dis = $('#display');
+    btn.click(function(){
+        let city = inp.value;
+        $
+    })
+})
