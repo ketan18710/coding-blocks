54b6986de3dc0fd35d2d0fc231ad085bc0358fd2
KetanVerma <Ketan18710@gmail.com>
2019-01-25 22:17:27 +0530

    fd

--- Lecture 11 .20-01-19/weather api/main.js
+++ Lecture 11 .20-01-19/weather api/main.js
@@ -1,15 +1,28 @@
-// let inp = document.getElementById('city_name');
-// let btn  = document.getElementById('btn');
-// let dis  = document.getElementById('display');
+// let inp = document.getElementById('city_name')
+// let btn  = document.getElementById('btn')
+// let dis  = document.getElementById('display')
 // btn.onclick = function(){
 
 // } 
 $(document).ready(function(){
-    let inp = $('#city_name');
-    let btn = $('#btn');
-    let dis = $('#display');
+    let inp = $('#city_name')
+    let btn = $('#button')
+    let dis = $('#display')
+    console.log('aaa')
     btn.click(function(){
-        let city = inp.value;
-        $
+        console.log('ww')
+        let city = inp.val()
+        let key = '6d7183eed5a64694bcd32919192401'
+        $.ajax({
+            url :`http://api.apixu.com/v1/current.json?key=${key}&q=${city}`,
+            method : 'GET',
+            success : function(data){
+                console.log(data.current.temp_c)
+            },
+            error: function(error){
+                console.log(error)
+            }
+        })
     })
 })
+
